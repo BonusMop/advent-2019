87ecb300a7294275cf29d5b0174a5b5a07d9adf2
Brian Butler <butler.brian.j@live.com>
2019-12-10 18:46:38 -0500

    Handle puzzle input

---
+++ src/Input/AdventInput.ts
@@ -0,0 +1,26 @@
+import * as fs from 'fs';
+
+export class AdventInput {
+
+    private _dataFolder: string; 
+
+    constructor (dataFolder: string) {
+        this._dataFolder = dataFolder;
+    }
+
+    async stringInputFor(day: number): Promise<string[]> {
+        return new Promise<string[]>((resolve, reject) => {
+            fs.readFile(`${this._dataFolder}/input.${day}.txt`, (err, data) => {
+                if (err) reject(err);
+                resolve(data.toString().split('\n'));
+            });
+        });
+    }
+
+    async inputFor<T>(day: number): Promise<T[]> {
+        return new Promise<T[]>( async resolve => {
+            resolve ((await this.stringInputFor(day)).map(d => JSON.parse(d) as T));
+        });
+    }
+
+}
